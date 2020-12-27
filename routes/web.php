6cb47d8c63d8d53e701f37fe99b5c1e5b027effb
John Azar <eng.johnazar@gmail.com>
2020-12-27 18:42:02 +0300

    middleware

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,12 +11,57 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/','FrontProductListController@index');
+Route::get('/product/{id}','FrontProductListController@show')->name('product.view');
+
+Route::get('/orders','CartController@order')->name('order')->middleware('auth');
+
+Route::get('/checkout/{amount}','CartController@checkout')->name('cart.checkout')->middleware('auth');
+
+Route::post('/charge','CartController@charge')->name('cart.charge');
+
+Route::get('/category/{name}','FrontProductListController@allProduct')->name('product.list');
+
+Route::get('/addToCart/{product}','CartController@addToCart')->name('add.cart');
+
+Route::get('/cart','CartController@showCart')->name('cart.show');
+
+Route::post('/products/{product}','CartController@updateCart')->name('cart.update');
+Route::post('/product/{product}','CartController@removeCart')->name('cart.remove');
+
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('category', 'CategoryController');
+Route::get('all/products','FrontProductListController@moreProducts')->name('more.product');
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('subcatories/{id}','ProductController@loadSubCategories');
+Route::group(['prefix'=>'auth','middleware'=>['auth','isAdmin']],function()
+{
+	Route::get('/dashboard', function () 
+	{
+		return view('admin.dashboard');
+	});
+
+	Route::resource('category','CategoryController');
+	Route::resource('subcategory','SubCategoryController');
+	Route::resource('product','ProductController');
+	//slider
+	Route::get('slider/create','SliderController@create')->name('slider.create');
+	Route::get('slider','SliderController@index')->name('slider.index');
+	Route::post('slider','SliderController@store')->name('slider.store');
+	Route::delete('slider/{id}','SliderController@destroy')->name('slider.destroy');
+
+	Route::get('users','UserController@index')->name('user.index');
+
+	//orders
+	Route::get('/orders','CartController@userOrder')->name('order.index');
+	Route::get('/orders/{userid}/{orderid}','CartController@viewUserOrder')->name('user.order');
+
+
+});
